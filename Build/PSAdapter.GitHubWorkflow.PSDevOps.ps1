033c30db6cd10df7fe6fbc9b1191ec1503f598d0
James Brundage <+@noreply.github.com>
2024-09-08 19:59:16 -0700

    feat: PSAdapter Workflow and Container Publishing ( Fixes #5, Fixes #12 )

---
+++ Build/PSAdapter.GitHubWorkflow.PSDevOps.ps1
@@ -0,0 +1,16 @@
+#requires -Module PSDevOps
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubWorkflow
+
+Push-Location ($PSScriptRoot | Split-Path)
+New-GitHubWorkflow -Name "Build PSAdapter" -On Push,
+    PullRequest, 
+    Demand -Job PowerShellStaticAnalysis, 
+    TestPowerShellOnLinux, 
+    TagReleaseAndPublish, BuildPSAdapter -Environment ([Ordered]@{
+        REGISTRY = 'ghcr.io'
+        IMAGE_NAME = '${{ github.repository }}'
+    }) -OutputPath .\.github\workflows\BuildPSAdapter.yml
+
+Pop-Location
