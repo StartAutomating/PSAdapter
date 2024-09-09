957f0f13d8b5181c730be948016297df8b0ca82e
James Brundage <+@noreply.github.com>
2024-09-08 23:11:44 -0700

    feat: PSAdapter basic formatting ( Fixes #19 )
    Grouping by PSAdapterType

--- Types/PSAdapter/PSAdapter.format.ps1
+++ Types/PSAdapter/PSAdapter.format.ps1
@@ -1,9 +1,9 @@
 Write-FormatView -TypeName PSAdapter -Action {
     Write-FormatViewExpression -ScriptBlock {
-        if ($_.BaseType.Name -match 'CmdletAdapter') {
+        if ($_.BaseType.Name -match 'CmdletAdapter' -or $_ -is [IO.FileInfo]) {
             $_.Fullname
         } else {
             $_.Name
         }        
     }
-}
+} -GroupByProperty PSAdapterType
